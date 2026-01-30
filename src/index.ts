b32d542167590dce6fbd70b0a909f508c48634dc
Talha Shahid Khan <157911509+TalhaShahidKhan@users.noreply.github.com>
2026-01-30 21:52:23 +0600

    Route Change

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(
 );
 
 app.use(express.json());
-app.all("/api/auth/*splat", toNodeHandler(auth));
+app.all("/api/v1/auth/*splat", toNodeHandler(auth));
 
 // API v1 routes
 app.use("/api/v1/student", studentRouter);
